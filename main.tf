c9a7e6bea9c8cf36fd55a5376decb2f176f351e9
OctaPod <95753192+OctaPod@users.noreply.github.com>
2023-02-25 18:13:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -523,6 +523,35 @@ resource "yandex_alb_load_balancer" "alb" {
   }
 }
 
+#####################################################---SNAPSHOT SCHEDULER---############################################################
+
+
+resource "yandex_compute_snapshot_schedule" "default" {
+  name           = "my-name"
+
+  schedule_policy {
+    expression = "45 15 ? * *"
+  }
+
+  snapshot_count = 7
+
+  snapshot_spec {
+      description = "snapshot-description"
+      labels = {
+        snapshot-label = "my-snapshot-label-value"
+      }
+  }
+
+  labels = {
+    my-label = "my-label-value"
+  }
+
+  disk_ids = ["${yandex_compute_instance.vm1.boot_disk.0.disk_id}", "${yandex_compute_instance.vm2.boot_disk.0.disk_id}", "${yandex_compute_instance.vm3.boot_disk.0.disk_id}", "${yandex_compute_instance.vm4.boot_disk.0.disk_id}", "${yandex_compute_instance.elastic.boot_disk.0.disk_id}", "${yandex_compute_instance.kibana.boot_disk.0.disk_id}", "${yandex_compute_instance.bastion.boot_disk.0.disk_id}"]
+}
+
+
+
+
 
 #####################################################---CREATE INVENTORY FILE FOR ANSIBLE *.INI ---###############
 
